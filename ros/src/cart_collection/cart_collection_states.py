df461878fe27c6f6b0fb55901acb24a615f3c391
Sebastian Blumenthal <blumenthal@locomotec.com>
2019-07-18 13:47:38 +0200

    Implemented final approche state.

--- ros/src/cart_collection/cart_collection_states.py
+++ ros/src/cart_collection/cart_collection_states.py
@@ -7,6 +7,13 @@ from tf.transformations import euler_from_quaternion, quaternion_from_euler
 import math
 import maneuver_navigation.msg
 
+def get_yaw_from_pose(pose):
+        orientation_q = pose.pose.orientation
+        orientation_list = [orientation_q.x, orientation_q.y, orientation_q.z, orientation_q.w]
+        (roll, pitch, yaw) = euler_from_quaternion (orientation_list)
+
+        return yaw
+
 def get_setpoint_in_front_of_pose(pose, distance):
     setpoint = None
     if(pose == None):
@@ -17,9 +24,7 @@ def get_setpoint_in_front_of_pose(pose, distance):
         rospy.logerr("Preconditon for get_setpoint_in_front_of_pose not met: Pose is not in map frame. Aborting.")
         return None
 
-    orientation_q = pose.pose.orientation
-    orientation_list = [orientation_q.x, orientation_q.y, orientation_q.z, orientation_q.w]
-    (roll, pitch, yaw) = euler_from_quaternion (orientation_list)
+    yaw = get_yaw_from_pose(pose)
 
     x_cart_in_world_frame = pose.pose.position.x
     y_cart_in_world_frame = pose.pose.position.y
@@ -152,7 +157,7 @@ class GoToPreDockSetpoint(smach.State):
                 break
 
         if self.robot_pose == None:
-            rospy.logerr("Preconditon for GoToPreDockSetpoint not met: Robot pose not available. Aborting.")
+            rospy.logerr("Precondition for GoToPreDockSetpoint not met: Robot pose not available. Aborting.")
             return 'timeout'
 
         # Send goal
@@ -195,10 +200,78 @@ class AlignAndApproachCart(smach.State):
                              outcomes=['approach_succeeded',
                                        'cart_not_found',
                                        'timeout'])
+        self.cart_pose_feedback_sub = rospy.Subscriber('/wm/docking_approach/cart_front', ropod_ros_msgs.msg.ObjectList, self.cart_front_pose_callback)
+        self.cmd_vel_pub = rospy.Publisher('/ropod/cmd_vel', geometry_msgs.msg.Twist, queue_size = 1)
+        self.cart_front_pose = None
         self.timeout = timeout
+        self.pose_reached =  False
 
     def execute(self, userdata):
-        return 'cart_not_found'
+        self.cart_front_pose = None
+
+        start_time = rospy.Time.now()
+        while (rospy.Time.now() - start_time <= self.timeout) and not self.pose_reached:
+            if self.cart_front_pose != None:
+                (vel, self.pose_reached) = self.calculate_final_approach_velocities(self.cart_front_pose)
+                cmd_vel_pub.publish(vel)
+            else:
+                rospy.logwarn("Precondition for AlignAndApproachCart not met: No cart_front_pose reveived so far. Retrying.")
+
+            rospy.sleep(0.1)
+
+        if self.pose_reached:
+            return 'approach_succeeded'
+
+        return 'timeout'
+
+    def cart_front_pose_callback(self, msg):
+        if(len(msg.objects) > 0):
+            self.cart_front_pose =  msg.objects[0]
+        if(len(msg.objects) > 1):
+            rospy.logwarn("Precondition for cart_front_pose_callback not met: Only one cart front beeing detected. Taking the first one.")
+
+    def calculate_final_approach_velocities(self, pose):
+        # Paremeters
+        BACKWARD_VEL_DOCKING = 0.1  # [m/s]
+        MAX_ROT_VEL_DOCKING = 0.1   # [rad/s]
+        x_thresh = 0.05             # [m/s]
+        y_thresh = 0.05             # [m/s]
+        yaw_thresh = 0.1            # [rad/s]
+
+        vel = geometry_msgs.msg.Twist()
+        vel.linear.x  = 0.0
+        vel.linear.y  = 0.0
+        vel.angular.z = 0.0
+        yaw = get_yaw_from_pose(pose)
+
+        if (abs(pose.pose.position.x) < x_thresh) \
+            and (abs(pose.pose.position.y) < y_thresh) \
+            and (abs(yaw) < yaw_thresh):
+            return vel, True # Reached / Done
+
+        if (abs(pose.pose.position.x) > x_thresh) \
+            and (abs(pose.pose.position.x) > abs(pose.pose.position.y)):
+                # magnitude of BACKWARD_VEL_DOCKING, sign of pose.pose.position.x
+                vel.linear.x = math.copysign(BACKWARD_VEL_DOCKING, pose.pose.position.x);
+                if (abs(pose.pose.position.y) > y_thresh):
+                    x_time = pose.pose.position.x / vel.linear.x;
+                    y_vel = pose.pose.position.y / x_time;
+                    vel.linear.y = math.copysign(y_vel, pose.pose.position.y);
+
+        if (abs(pose.pose.position.y) > y_thresh) \
+            and (abs(pose.pose.position.y) > abs(pose.pose.position.x)):
+                vel.linear.y = math.copysign(BACKWARD_VEL_DOCKING, pose.pose.position.y);
+                if (abs(pose.pose.position.x) > x_thresh):
+                    y_time = pose.pose.position.y / vel.linear.y;
+                    x_vel = pose.pose.position.x / y_time;
+                    vel.linear.x = math.copysign(x_vel, pose.pose.position.x);
+
+        if (math.abs(yaw) > 0.1):
+            # magnitude of MAX_ROT_VEL_DOCKING, sign of yaw
+            vel.angular.z = maht.copysign(MAX_ROT_VEL_DOCKING, yaw);
+
+        return vel, False # Not yet done, thus False
+
 
 class CoupleToCart(smach.State):
     def __init__(self, timeout=5.0):
