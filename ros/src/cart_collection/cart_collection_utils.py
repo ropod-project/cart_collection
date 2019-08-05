2b711bdf05837ecc0968b26fbb64f75b2f5ae182
Santosh Thoduka <santosh.thoduka@gmail.com>
2019-08-05 18:35:45 +0200

    use geometry of local area to derive orientation of cart
    This is based on the assumption that the cart is placed against a
    wall/edge in the local area specified in the task.
    We first find the edge in the local area closest to the pose of
    the cart, then set the pose of the cart as close to the orientation
    of the normal of the edge (in 90 degree offsets from the original
    cart orientation).

--- ros/src/cart_collection/cart_collection_utils.py
+++ ros/src/cart_collection/cart_collection_utils.py
@@ -1,9 +1,11 @@
 import math
+import numpy as np
 
 import rospy
 import dynamic_reconfigure.client
-from tf.transformations import euler_from_quaternion
+from tf.transformations import euler_from_quaternion, quaternion_from_euler
 from geometry_msgs.msg import PoseStamped
+import ropod_ros_msgs.msg
 
 def get_yaw_from_pose(pose):
     orientation_q = pose.pose.orientation
@@ -30,7 +32,7 @@ def get_setpoint_in_front_of_pose(pose, distance):
     setpoint.pose.position.z = pose.pose.position.z
     setpoint.pose.orientation = pose.pose.orientation
 
-    return  setpoint
+    return setpoint
 
 def set_omni_drive_mode():
     node_name = '/maneuver_navigation/TebLocalPlannerROS'
@@ -40,6 +42,7 @@ def set_omni_drive_mode():
        rospy.logerr("Service {0} does not exist".format(node_name + '/set_parameters'))
        return False
 
+    # TODO: these parameters need to be read from the launch file
     params = {"max_vel_y" : 0.5,
               "weight_kinematics_nh": 0,
               "weight_kinematics_forward_drive": 0}
@@ -56,6 +59,7 @@ def reset_to_non_holonomic_mode():
        rospy.logerr("Service {0} does not exist".format(node_name + '/set_parameters'))
        return False
 
+    # TODO: these parameters need to be read from the launch file
     params = {"max_vel_y" : 0.0,
               "weight_kinematics_nh": 1000,
               "weight_kinematics_forward_drive": 1}
@@ -63,3 +67,108 @@ def reset_to_non_holonomic_mode():
         config = client.update_configuration(params)
     except Exception as e:
         rospy.logerr("Failed to set dynamic reconfigure params for " + node_name)
+
+def get_distance_to_line(p1, p2, tp):
+    # https://en.wikipedia.org/wiki/Distance_from_a_point_to_a_line
+    numerator = np.abs((p2.y - p1.y) * tp.x - (p2.x - p1.x) * tp.y + p2.x*p1.y - p2.y*p1.x)
+    denominator  = np.sqrt((p2.y - p1.y)**2 + (p2.x - p1.x)**2)
+    return numerator / denominator
+
+
+def does_point_intersect_segment_on_right(point, segment_p1, segment_p2):
+    # find intersection point and check if t.x is to the left
+
+    # t.x < p1.x + ((p1.x - p1.x)*(t.y - p1.y) / (p2.y - p1.y)
+    return (point.x <
+            segment_p1.x +
+            ((segment_p2.x - segment_p1.x) * (point.y - segment_p1.y) /
+            (segment_p2.y - segment_p1.y)))
+
+def is_point_between_points_in_y(point, segment_p1, segment_p2):
+    # check if the sign of the difference between the y coordinates of the points is different
+    sign1 = math.copysign(1.0, point.y - segment_p1.y)
+    sign2 = math.copysign(1.0, point.y - segment_p2.y)
+    if (sign1 == sign2):
+        return False
+    return True
+
+def is_point_in_polygon(point, polygon):
+    # Based on algorithm here:
+    # http://www.eecs.umich.edu/courses/eecs380/HANDOUTS/PROJ2/InsidePoly.html
+    #
+    # If a horizontal ray from the test point to the right intersects with
+    # each segment of the polygon an even number of times, it is not in the polygon
+
+    num_intersections = 0
+    for idx, p in enumerate(polygon[:-1]):
+        if (is_point_between_points_in_y(point, p, polygon[idx+1]) and
+            does_point_intersect_segment_on_right(point, p, polygon[idx+1])):
+            num_intersections += 1
+    if num_intersections % 2 == 0:
+        return False
+    return True
+
+def get_pose_closest_to_normal(input_pose, edge_normal):
+    pose_yaw = get_yaw_from_pose(input_pose)
+    quadrants = [pose_yaw, pose_yaw + np.pi/2.0, pose_yaw - np.pi/2.0, pose_yaw + np.pi]
+    min_angle = 1000.0
+    closest_yaw = pose_yaw
+    for quad in quadrants:
+        angle = np.arctan2(np.sin(quad - edge_normal), np.cos(quad - edge_normal))
+        if np.abs(angle) < min_angle:
+            min_angle = np.abs(angle)
+            closest_yaw = quad
+    closest_yaw = np.arctan2(np.sin(closest_yaw), np.cos(closest_yaw))
+    output_pose = PoseStamped()
+    output_pose.header = input_pose.header
+    output_pose.pose.position = input_pose.pose.position
+    q = quaternion_from_euler(0.0, 0.0, closest_yaw)
+    output_pose.pose.orientation.x = q[0]
+    output_pose.pose.orientation.y = q[1]
+    output_pose.pose.orientation.z = q[2]
+    output_pose.pose.orientation.w = q[3]
+    return output_pose
+
+def get_pose_perpendicular_to_edge(shape, input_pose):
+    closest_edge_idx = 0
+    min_dist = 1000.0;
+    input_point = ropod_ros_msgs.msg.Position()
+    input_point.x = input_pose.pose.position.x
+    input_point.y = input_pose.pose.position.y
+    for idx, v in enumerate(shape.vertices[:-1]): # last and first vertex are the same
+        dist = get_distance_to_line(v, shape.vertices[idx+1], input_point)
+        if dist < min_dist:
+            min_dist = dist
+            closest_edge_idx = idx
+
+    # this is the edge closest to the input_pose
+    v1 = shape.vertices[closest_edge_idx]
+    v2 = shape.vertices[closest_edge_idx + 1]
+
+    dx = (v2.x - v1.x)
+    dy = (v2.y - v1.y)
+    # normal of edge pointing inwards (assuming anticlockwise order of points)
+    enx = -dy
+    eny = dx
+
+    # check if normal points inside the shape by testing
+    # if we end up inside the polygon by going along the normal
+    # This check is necessary because the assumption that the polygon
+    # points are given in anticlockwise order is not true
+    # The ordering is dependent on how the OSM map was created; hence
+    # it could be either CW or CCW. If it was CW, we need to flip the normal
+    edge_normal = np.arctan2(eny, enx)
+    test_point = ropod_ros_msgs.msg.Position()
+    test_point.x = (v1.x + v2.x) / 2.0 + 0.01*np.cos(edge_normal)
+    test_point.y = (v1.y + v2.y) / 2.0 + 0.01*np.sin(edge_normal)
+    if (not is_point_in_polygon(test_point, shape.vertices)):
+        # the normal was pointing away from the polygon, so we need to flip it
+        enx = -enx
+        eny = -eny
+
+    edge_normal = np.arctan2(eny, enx)
+    # Change the direction of input_pose by increments of pi/2 until we find
+    # the one closest to the normal of edge
+    output_pose = get_pose_closest_to_normal(input_pose, edge_normal)
+
+    return output_pose
--- ros/src/cart_collection/get_cart_pose.py
+++ ros/src/cart_collection/get_cart_pose.py
@@ -4,6 +4,9 @@ import actionlib
 
 from geometry_msgs.msg import PoseStamped
 from ropod_ros_msgs.msg import GetObjectsAction, GetObjectsGoal
+from ropod_ros_msgs.msg import GetShapeAction, GetShapeGoal
+
+from cart_collection.cart_collection_utils import get_pose_perpendicular_to_edge
 
 class GetCartPose(smach.State):
     def __init__(self, timeout=5.0,
@@ -12,21 +15,24 @@ class GetCartPose(smach.State):
                              input_keys=['cart_sub_area'],
                              output_keys=['cart_pose'])
         self.get_objects_client = actionlib.SimpleActionClient("get_objects", GetObjectsAction)
+        self.get_shape_client = actionlib.SimpleActionClient("get_shape", GetShapeAction)
         self.cart_pose_pub = rospy.Publisher("cart_pose", PoseStamped, queue_size=1)
-        self.timeout = timeout
+        self.timeout = rospy.Duration.from_sec(timeout)
         self.map_frame_name = map_frame_name
 
     def execute(self, userdata):
-        action_server_available = self.get_objects_client.wait_for_server(timeout=rospy.Duration(self.timeout))
-        if not action_server_available:
+        obj_action_server_available = self.get_objects_client.wait_for_server(timeout=self.timeout)
+        shape_action_server_available = self.get_shape_client.wait_for_server(timeout=self.timeout)
+        if not obj_action_server_available or not shape_action_server_available:
             return 'timeout'
 
         goal = GetObjectsGoal()
         goal.area_id = userdata.cart_sub_area
         goal.type = 'carts'
         self.get_objects_client.send_goal(goal)
-        result = self.get_objects_client.wait_for_result(timeout=rospy.Duration(self.timeout))
+        result = self.get_objects_client.wait_for_result(timeout=self.timeout)
         if not result:
+            rospy.logerr("[cart_collector] Timed out waiting for cart object")
             return 'timeout'
 
         res = self.get_objects_client.get_result()
@@ -38,7 +44,20 @@ class GetCartPose(smach.State):
             return 'cart_not_found'
 
         rospy.loginfo("[cart_collector] # of found objects = " + str(len(res.objects)))
-        self.cart_pose_pub.publish(res.objects[0].pose)
-        userdata.cart_pose = res.objects[0].pose # fixme
+
+        goal = GetShapeGoal()
+        goal.id = int(userdata.cart_sub_area)
+        goal.type = 'local_area'
+        self.get_shape_client.send_goal(goal)
+        shape_success = self.get_shape_client.wait_for_result(timeout=self.timeout)
+        if not shape_success:
+            rospy.logerr("[cart_collector] Timed out waiting for shape of docking area")
+            return 'timeout'
+        shape_result = self.get_shape_client.get_result()
+
+        cart_pose = get_pose_perpendicular_to_edge(shape_result.shape, res.objects[0].pose)
+        ## NOTE: we assume there is only one cart in the specified sub area
+        self.cart_pose_pub.publish(cart_pose)
+        userdata.cart_pose = cart_pose
 
         return 'cart_found'
